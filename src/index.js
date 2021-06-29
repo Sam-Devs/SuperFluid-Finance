021fa628ba104b63f8eacecbbdf922e5e07a1597
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-06-29 14:31:22 +0100

    index file updated

--- src/index.js
+++ src/index.js
@@ -1,24 +1,60 @@
 const SuperfluidSDK = require("@superfluid-finance/js-sdk");
 const Web3 = require("web3");
 
-const superfluid = new SuperfluidSDK.Framework({
-    web3: new Web3(window.ethereum)
-})
- superfluid.initialize();
+const superfluid = async () => {
+  try {
+    const sf = new SuperfluidSDK.Framework({
+      web3: new Web3(
+        "https://eth-goerli.alchemyapi.io/v2/gYsNxGzbVTOC0D_liQiO-e4vqPIDC18w"
+      ),
+    });
+    await sf.initialize();
 
-const wallet =  window.ethereum.request({
-    method: "request_ethereum_wallet_account",
-    params: [
-        {
-            ethereum_accounts: []
-        }
-    ]
-})
+    const walletAddress = "0x9C0cEC1f95582100b07581efCEB45A96FE01e8C0";
 
-const sam = wallet.user({
-    address: wallet[0],
-    token: "0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00"
-})
+    const carol = sf.user({
+      address: walletAddress,
+      token: "0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00",
+    });
 
-const accountDetails = sam.details();
-console.log(accountDetails);
+    await carol.flow({
+      recipient: "0x52DBDfa46Cd6AeC23e248c3D2cec723830D227CD",
+      flowRate: "385802469135802",
+    });
+
+    const details = await carol.details();
+    console.log(details);
+  } catch (error) {
+    console.log(error);
+  }
+};
+superfluid();
+
+// const sf = new SuperfluidSDK.Framework({
+//   web3: new Web3("wss://goerli.infura.io/ws/v3/ba5697fa2ff24490a41a7bf8991a7799"),
+// });
+// await sf.initialize();
+
+// const walletAddress =  window.ethereum.request({
+//   method: "eth_requestAccounts",
+//   params: [
+//     {
+//       eth_accounts: {},
+//     },
+//   ],
+// });
+
+// const walletAddress = "0x9C0cEC1f95582100b07581efCEB45A96FE01e8C0";
+
+// const carol = sf.user({
+//   address: walletAddress,
+//   token: "0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00",
+// });
+
+// carol.flow({
+//   recipient: "0x52DBDfa46Cd6AeC23e248c3D2cec723830D227CD",
+//   flowRate: "385802469135802",
+// });
+
+// const details = await carol.details();
+// console.log(details);
